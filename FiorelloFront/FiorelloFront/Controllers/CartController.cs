e51a9334f0212301be5b33e7bdcbf22c86d47da4
anarmh <anarmh@code.edu.az>
2023-05-22 23:00:53 +0400

    add basket count

---
+++ FiorelloFront/FiorelloFront/Controllers/CartController.cs
@@ -0,0 +1,122 @@
+﻿using FiorelloFront.Data;
+using FiorelloFront.Models;
+using FiorelloFront.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NuGet.ContentModel;
+
+namespace FiorelloFront.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IHttpContextAccessor _accessor;
+        public CartController(AppDbContext context, IHttpContextAccessor accessor)
+        {
+            _context = context;
+            _accessor = accessor;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            List<BasketDetailVM> basketList = new();
+            if (_accessor.HttpContext.Request.Cookies["basket"] != null)
+            {
+                List<BasketVM> basketDatas = JsonConvert.DeserializeObject<List<BasketVM>>(_accessor.HttpContext.Request.Cookies["basket"]);
+                foreach (var item in basketDatas)
+                {
+                    var dbProduct = await _context.Products.Include(m => m.ProductImages).FirstOrDefaultAsync(m=>m.Id==item.Id);
+
+                    if (dbProduct != null)
+                    {
+                        BasketDetailVM basketDetail = new()
+                        {
+                            Id = dbProduct.Id,
+                            Image = dbProduct.ProductImages.Where(m => m.IsMain).FirstOrDefault().Image,
+                            Name = dbProduct.Name,
+                            Count = item.Count,
+                            Price = dbProduct.Price,
+                            TotalPrice = item.Count * dbProduct.Price,
+                        };
+                        basketList.Add(basketDetail);
+
+                    }
+                 
+                }
+               
+            } 
+            return View(basketList);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Addbasket(int? id)
+        {
+            if (id is null) return BadRequest();
+            Product product = await _context.Products.FindAsync(id);
+
+            if(product == null) return NotFound();
+
+            List<BasketVM> basket = GetBasketDatas();
+            
+            AddProductToBasket(basket,product);
+
+            return Ok(basket.Sum(m=>m.Count));
+        }
+
+
+        private List<BasketVM> GetBasketDatas()
+        {
+            List<BasketVM> basket;
+            if (_accessor.HttpContext.Request.Cookies["basket"] != null)
+            {
+                basket = JsonConvert.DeserializeObject<List<BasketVM>>(_accessor.HttpContext.Request.Cookies["basket"]);
+            }
+            else
+            {
+                basket = new List<BasketVM>();
+            }
+            return basket;
+
+        }
+
+        private void AddProductToBasket(List<BasketVM> basket, Product product)
+        {
+            BasketVM existProduct = basket.FirstOrDefault(m => m.Id == product.Id);
+
+            if (existProduct == null)
+            {
+                basket.Add(new BasketVM
+                {
+                    Id = product.Id,
+                    Count = 1
+                });
+            }
+            else
+            {
+                existProduct.Count++;
+            }
+
+
+            _accessor.HttpContext.Response.Cookies.Append("basket", JsonConvert.SerializeObject(basket));
+
+           
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeleteProductFrombasket(int? id)
+        {
+            List<BasketVM> basketDatas = JsonConvert.DeserializeObject<List<BasketVM>>(_accessor.HttpContext.Request.Cookies["basket"]);
+
+            var data=basketDatas.FirstOrDefault(m=>m.Id==id);
+
+            basketDatas.Remove(data);
+
+            _accessor.HttpContext.Response.Cookies.Append("basket",JsonConvert.SerializeObject(basketDatas));
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ FiorelloFront/FiorelloFront/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿
+using FiorelloFront.Data;
+using FiorelloFront.Models;
+using FiorelloFront.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Linq;
+
+namespace FiorelloFront.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IHttpContextAccessor _accessor;
+
+        public HomeController(AppDbContext context, IHttpContextAccessor accessor)
+        {
+            _context = context;
+            _accessor = accessor;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+           IEnumerable<Slider> sliders=await _context.Sliders.Where(m=>!m.SoftDelete).ToListAsync();
+            SliderInfo sliderInfo= await _context.SliderInfos.Where(m=>!m.SoftDelete).FirstOrDefaultAsync();
+            IEnumerable<Blog> blogs=await _context.Blogs.Where(m => !m.SoftDelete).OrderByDescending(m=>m.Id).Take(3).ToListAsync();
+            IEnumerable<Category> categories=await _context.Categories.Where(m => !m.SoftDelete).ToListAsync();
+            IEnumerable<Product> products = await _context.Products.Include(m=>m.ProductImages).Take(8).Where(m => !m.SoftDelete).ToListAsync();
+            About about=await _context.Abouts.Where(m => !m.SoftDelete).FirstOrDefaultAsync();
+            IEnumerable<FlowersExpert> flowersExperts = await _context.FlowersExperts.Where(m => !m.SoftDelete).ToListAsync();
+            IEnumerable<Say> says = await _context.Says.Where(m => !m.SoftDelete).ToListAsync();
+            IEnumerable<Instagram> instagrams = await _context.Instagrams.Where(m => !m.SoftDelete).ToListAsync();
+            HomeVM model = new()
+            {
+                SliderInfo = sliderInfo,
+                Sliders = sliders,
+                Blogs=blogs,
+                Categories= categories,
+                Products= products,
+               About= about,
+               FlowersExperts= flowersExperts,
+               Says= says,
+               Instagrams= instagrams
+            };
+
+            return View(model);
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Addbasket(int? id)
+        //{
+        //    if (id is null) return BadRequest();
+        //    Product product = await _context.Products.FindAsync(id);
+
+        //    if (product == null) return NotFound();
+
+        //    List<BasketVM> basket = GetbasketDatas();
+
+        //    AddProductToBasket(basket, product);
+
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+
+
+        //private List<BasketVM> GetbasketDatas()
+        //{
+        //    List<BasketVM> basket;
+        //    if (_accessor.HttpContext.Request.Cookies["basket"] != null)
+        //    {
+        //        basket = JsonConvert.DeserializeObject<List<BasketVM>>(_accessor.HttpContext.Request.Cookies["basket"]);
+        //    }
+        //    else
+        //    {
+        //        basket = new List<BasketVM>();
+        //    }
+        //    return basket;
+
+        //}
+
+
+        //private void AddProductToBasket(List<BasketVM> basket, Product product)
+        //{
+        //    BasketVM existProduct = basket.FirstOrDefault(m => m.Id == product.Id);
+
+        //    if (existProduct == null)
+        //    {
+        //        basket.Add(new BasketVM
+        //        {
+        //            Id = product.Id,
+        //            Count = 1
+        //        });
+        //    }
+        //    else
+        //    {
+        //        existProduct.Count++;
+        //    }
+
+
+        //    _accessor.HttpContext.Response.Cookies.Append("basket", JsonConvert.SerializeObject(basket));
+        //}
+
+
+
+
+
+
+    }
+}
